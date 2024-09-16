06fa1aff4772631f49c07d4cd6d32c74ba626663
kaua-araujo <araujo.ka37@gmail.com>
2024-09-16 17:58:41 -0300

    feat:crud

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,5 +1,5 @@
 import House from '../models/House';
-
+import User from '../models/User';
 class HouseController{
 
     async index(req, res)
@@ -18,7 +18,7 @@ class HouseController{
         const { user_id } = req.headers;
 
         const house = await House.create({
-            use: user_id,
+            user: user_id,
             thumbnail: filename,
             description,
             price,
@@ -35,7 +35,14 @@ class HouseController{
         const { description, price, location, status } = req.body;
         const { user_id } = req.headers;
 
-        const houses = await House.updateOne({ _id: house_id},{
+        const user = await User.findById(user_id);
+        const houses = await House.findById(house_id);
+
+        if (String(user._id) !== String(houses.user)) {
+            return res.status(401).json({error: 'Não autorizado.'});
+        };
+
+        await House.updateOne({ _id: house_id},{
             use: user_id,
             thumbnail: filename,
             description,
@@ -43,7 +50,26 @@ class HouseController{
             location,
             status,
         });
-        return res.json( houses );
+
+
+        return res.send();
+    }
+
+    async destroy(req, res)
+    {
+        const { house_id } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+        const houses = await House.findById(house_id);
+
+        if (String(user._id) !== String(houses.user)) {
+            return res.status(401).json({error: 'Não autorizado.'});
+        };
+
+        await House.findByIdAndDelete({ _id: house_id});
+
+        return res.json({message:"Exluida com sucesso!"});
     }
 
 }
--- src/routes.js
+++ src/routes.js
@@ -3,15 +3,23 @@ import multer from 'multer';
 import uploadConfig from './config/upload';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController';
+import DashboardController from './controllers/DashboardController';
 
 
 const routes = new Router();
 const upload = multer(uploadConfig);
 
+//Sessões
 routes.post('/sessions', SessionController.store);
 
+// Casas
 routes.get('/houses', HouseController.index);
 routes.post('/houses', upload.single('thumbnail'),HouseController.store);
 routes.put('/houses/:house_id', upload.single('thumbnail'),HouseController.update);
+routes.delete('/houses', HouseController.destroy);
+
+// Dashboard
+routes.get('/dashboard', DashboardController.show);
+
 
 export default routes;
